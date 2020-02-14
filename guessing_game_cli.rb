e1c801af9a65bdf58108d5cca3f6a06dc6a72975
Brandon Green <brandon@brandonleeproductions.com>
2020-02-14 23:31:25 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,7 +3,7 @@ def run_guessing_game
   puts "Please guess a number between 1 and 6."
   
   input = gets.chomp 
-  if input == random_num
+  if input == random_num.to_s
     puts 'You guessed the correct number!'
   elsif input == 'exit'
     puts 'Goodbye!'
